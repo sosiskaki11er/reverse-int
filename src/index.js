1c38b4534c2cc4d83aedaa06d4272eb7028bff02
sosiskaki11er <93476714+sosiskaki11er@users.noreply.github.com>
2022-07-02 21:01:44 +0500

    feat:added reverse-int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+let newInt='';
+let intStr=number.toString();
+for(let i=1;i<=number.toString().length;i++){
+newInt=newInt+intStr[intStr.length-i];
+}
+return +(Number(newInt));
 }
